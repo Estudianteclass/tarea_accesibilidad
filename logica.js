5e3ff5b5d6ffe5c4edfbf660987b6455afcd334c
gabriel <2DAW10@DOMDPTOINFJMPP.INF>
2025-01-20 18:20:06 +0000

    cambios lunes

--- logica.js
+++ logica.js
@@ -17,12 +17,18 @@ movingText.addEventListener('mouseout', (e) => {
 let fuente=document.querySelector('select');
 console.log(fuente.value)
 
+let formulario=document.getElementById("formulario")
+let imagen=document.getElementById("foto1")
 fuente.addEventListener('click',(e)=>{
 
 if (fuente.value=='grande') {
     document.body.style.fontSize="20px";
+    formulario.setAttribute("class","flex flex-col w-96 h-64 justify-self-center border justify-center rounded-md px-4 space-y-3")
+    imagen.setAttribute("class","w-64 w-64 rounded-md")
 }else{
     document.body.style.fontSize="";
+    formulario.setAttribute("class","flex flex-col w-96 h-52 justify-self-center border justify-center rounded-md px-4 space-y-3")
+    imagen.setAttribute("class","w-52 w-52 rounded-md")
 }
 
 })
