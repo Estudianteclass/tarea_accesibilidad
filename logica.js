42f520ecc2c67433b0618af47a085dc768297122
gabriel <gabrielpr9520@gmail.com>
2025-01-23 09:00:41 +0000

    cambios finales

--- logica.js
+++ logica.js
@@ -19,16 +19,22 @@ let fuente=document.querySelector('select');
 
 let formulario=document.getElementById("formulario")
 let imagen=document.getElementById("foto1")
+let subTexto=document.getElementById("subtexto")
+let introduccion=document.getElementById("introduccion")
 fuente.addEventListener('click',(e)=>{
 
 if (fuente.value=='grande') {
     document.body.style.fontSize="20px";
-    formulario.setAttribute("class","flex flex-col w-96 h-64 justify-self-center border justify-center rounded-md px-4 space-y-3")
+    formulario.setAttribute("class","flex flex-col w-1/2 h-full p-6 justify-self-center border justify-center rounded-md px-4 space-y-3 border-pink-700 bg-pink-100 md:h-full w-72")
     imagen.setAttribute("class","w-64 w-64 rounded-md")
+   subTexto.setAttribute("class","text-center text-xl")
+   introduccion.setAttribute("class","text-3xl text-center py-1")
 }else{
     document.body.style.fontSize="";
-    formulario.setAttribute("class","flex flex-col w-96 h-52 justify-self-center border justify-center rounded-md px-4 space-y-3")
-    imagen.setAttribute("class","w-52 w-52 rounded-md")
+    formulario.setAttribute("class","flex flex-col w-1/2 h-full p-6 justify-self-center border justify-center rounded-md px-4 space-y-3 border-pink-700 bg-pink-100 md:h-full w-72")
+    imagen.setAttribute("class","w-64  h-64 rounded-md")
+    subTexto.setAttribute("class","text-center text-xs")
+    introduccion.setAttribute("class","text-2xl text-center py-2")
 }
 
 })
